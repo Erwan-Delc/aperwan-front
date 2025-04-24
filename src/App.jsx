e07b47604aed7a5017ac1b2c15af9a4e64817fe7
Erwan-Delc <delcroix.erwan@hotmail.fr>
2025-04-24 13:22:20 +0200

    🎉 Initial commit - Setup Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur Aper'wan 🍷🧀</h1>
+      <p>Le projet commence ici !</p>
+    </div>
+  );
+}
+
+export default App;
